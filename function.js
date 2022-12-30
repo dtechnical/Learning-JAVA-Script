3d0a4b97bfc51441944da7c192b34ee2230398b2
David_Chigbo <david.chigbo25@gmail.com>
2022-12-30 13:14:50 -0800

    function.js

---
+++ function.js
@@ -0,0 +1,29 @@
+
+/*Function is a set of statement 
+that performs a task or calculate a value.*/
+
+function addNumbers (number2, number1,) {
+    var addition =number2 + number1;
+    return addition;
+}
+
+//output  arguments......
+var result1 = addNumbers (15, 10);
+var result2 = addNumbers (29, 31);
+
+//output resut
+console.log(result1);
+console.log(result2);
+console.log(result1);
+
+/*Built in function*/
+
+var person = {
+    name: "dtechnical"
+}
+console.log(Object.values(person));
+console.log(Object.keys(person));
+console.log("dtechnical".toUpperCase());
+console.log("dtechnical".toLocaleLowerCase());
+console.log("dtechnical".indexOf("i"));
+console.log("dtechnical".length);
